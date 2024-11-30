138b3bc431388371f44947bff1cb2f8bdc283658
max <153589599+m4xkub@users.noreply.github.com>
2024-11-30 22:12:59 +0700

    Setting up a Fiber engine and add todo

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -1,14 +1,20 @@
 package cli
 
 import (
+	"food-order/internal/controller"
 
 	"github.com/spf13/cobra"
 )
 
 var RootCmd = &cobra.Command{
 	Use:   "Root",
-	Short: "Root command used as an entry point",
+	Short: "Root command used as an entry point for starting application",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		r := controller.GetRootController()
+
+		if err := r.Listen(":3000"); err != nil {
+			return err
+		}
 		return nil
 	},
 }
