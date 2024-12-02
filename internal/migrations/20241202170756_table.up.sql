e569e91167457c3393f35eadff603df4fc990292
max <153589599+m4xkub@users.noreply.github.com>
2024-12-02 23:29:15 +0700

    change table's name, status and added total_price as a field

--- internal/migrations/20241202170756_table.up.sql
+++
@@ -1,9 +0,0 @@
---SQL migration content here
-CREATE TABLE
-    IF NOT EXISTS table_info (
-        id UUID PRIMARY KEY,
-        table_number INTEGER NOT NULL,
-        status TEXT CHECK (status IN ('paid', 'occupied', 'reserved')),
-        time_in TIMESTAMP DEFAULT NOW () NOT NULL,
-        time_out TIMESTAMP
-    );
