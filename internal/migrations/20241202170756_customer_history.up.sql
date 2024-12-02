a58cdc1c2a730d7fe29f5809435b729275d7673f
max <153589599+m4xkub@users.noreply.github.com>
2024-12-02 23:53:49 +0700

    making id column auto generated

--- internal/migrations/20241202170756_customer_history.up.sql
+++ internal/migrations/20241202170756_customer_history.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE
     IF NOT EXISTS customer_history (
-        id UUID PRIMARY KEY,
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
         table_number INTEGER NOT NULL,
         status TEXT NOT NULL DEFAULT 'occupied' CHECK (status IN ('paid', 'occupied')),
         time_in TIMESTAMP NOT NULL DEFAULT NOW (),
